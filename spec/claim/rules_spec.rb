36bd99ef3c0d34b757fbaac6c456e42c6774848c
John Kisor <jkisor@gmail.com>
2019-05-09 22:21:59 -0400

    [tcr]

--- spec/claim/rules_spec.rb
+++ spec/claim/rules_spec.rb
@@ -1,12 +1,19 @@
 class Claim
   class Rules
+    
+    def allow?(board, space)
+    end
 
   end
+
 end
 
 describe Claim::Rules do
-  
+  let(:board) { double }
+  let(:space) { double }
+
   it do
+    subject.allow?(board, space)
   end
 
 end
