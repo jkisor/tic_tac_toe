17b47e0f489d79874d28c1e10ac3295f025a053a
John Kisor <jkisor@gmail.com>
2019-05-09 21:59:46 -0400

    [tcr]

--- spec/untitled_spec.rb
+++ spec/untitled_spec.rb
@@ -6,8 +6,8 @@ class Claim
   end
 
 
-  def call
-    @commit.() if @rules.allow?
+  def call(board)
+    @commit.(board) if @rules.allow?
     
   end
 
@@ -19,19 +19,21 @@ describe Claim do
 
   let(:rules)  { double(:allow? => true) }
   let(:commit) { double(:call => nil) }
+
+  let(:board) { double }
   
-  before { claim.() }
+  before { claim.(board) }
   
   it "checks rules" do
     expect(rules).to have_received(:allow?)
   end
 
-  it "commits the claim" do
-    expect(commit).to have_received(:call)
+  it "commits the move" do
+    expect(commit).to have_received(:call).with(board)
   end
 
   context "against the rules" do
-  let(:rules)  { double(:allow? => false) }
+    let(:rules)  { double(:allow? => false) }
 
     it "doesn't commits the claim" do
       expect(commit).not_to have_received(:call)
