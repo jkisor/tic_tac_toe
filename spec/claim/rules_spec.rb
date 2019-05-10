74201bac3907c1dd779bae63db8e6ac0805a5dfc
John Kisor <jkisor@gmail.com>
2019-05-09 22:27:37 -0400

    [tcr]

--- spec/claim/rules_spec.rb
+++ spec/claim/rules_spec.rb
@@ -13,8 +13,12 @@ describe Claim::Rules do
   let(:empty_board) { "---------" }
   let(:space) { 0 }
 
-  it "is allowed" do
-    expect(subject.allow?(empty_board, space)).to eq(true)
+  context "space is empty" do
+    
+    it "is allowed" do
+      expect(subject.allow?(empty_board, space)).to eq(true)
+    end
+
   end
 
 end
