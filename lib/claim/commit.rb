b066f66cae6e96a09a15509bb5544753c85b94dc
John Kisor <jkisor@gmail.com>
2019-05-11 09:08:13 -0400

    [tcr]

--- lib/claim/commit.rb
+++ lib/claim/commit.rb
@@ -3,8 +3,11 @@ class Claim
   class Commit
 
     def call(board, space, shape)
-      board[space] = shape
-      board
+
+      board.dup.tap do |new_board|
+        new_board[space] = shape
+      end
+
     end
 
   end
