fd2db6a3bbcc9a2b1c5ac504a4551f2f72fb20e5
John Kisor <jkisor@gmail.com>
2019-05-11 17:04:22 -0400

    [tcr]

--- spec/scratch_spec.rb
+++ spec/scratch_spec.rb
@@ -1,6 +1,34 @@
-describe "" do
+
+class FormatBoard
+  
+  def call(board)
+    space_rows = board.chars.each_slice(3).to_a.map do |row|
+      "| #{row.join(" | ")} |"
+    end
+    
+    [
+      " ----------- ",
+      space_rows,
+      " ----------- ",
+    ].flatten.join("\n")
+
+  end
+
+end
+
+describe FormatBoard do
   
   it "" do
+    input  = "XOXOXOXOX"
+    output = <<~BOARD.chomp
+       ----------- 
+      | X | O | X |
+      | O | X | O |
+      | X | O | X |
+       ----------- 
+    BOARD
+    
+    expect( FormatBoard.new.(input) ).to eq(output)
   end
 
 end
