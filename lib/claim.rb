f796da59d149f7b5819376955146678522cb2914
John Kisor <jkisor@gmail.com>
2019-05-11 08:53:11 -0400

    [tcr]

--- lib/claim.rb
+++ lib/claim.rb
@@ -1,15 +1,15 @@
 class Claim
 
-  def initialize(rules, determine_shape, commit)
+  def initialize(rules, next_shape, commit)
     @rules  = rules
-    @determine_shape = determine_shape
+    @next_shape = next_shape
     @commit = commit
   end
 
   def call(board, space)
     return board unless @rules.allow?(board, space)
 
-    shape = @determine_shape.(board)
+    shape = @next_shape.(board)
     @commit.(board, space, shape) 
   end
 
