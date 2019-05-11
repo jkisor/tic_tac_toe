c44a5836e607adb2bc4ce6bed0d04b56ffb002bf
John Kisor <jkisor@gmail.com>
2019-05-11 07:37:33 -0400

    [tcr]

--- lib/claim/commit.rb
+++ lib/claim/commit.rb
@@ -1,7 +1,11 @@
 class Claim
-  
+
   class Commit
-  
+
+    def call(board, space)
+      
+    end
+
   end
 
 end
--- spec/claim/commit_spec.rb
+++ spec/claim/commit_spec.rb
@@ -1,4 +1,11 @@
 require "./lib/claim/commit"
 
 describe Claim::Commit do
+  
+  it do
+    board = double
+    space = double
+    subject.call(board, space)
+  end
+
 end
