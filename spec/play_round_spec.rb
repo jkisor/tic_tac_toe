de0f0ccc58977f4ee28759cf1645b867eff44be9
John Kisor <jkisor@gmail.com>
2019-05-12 12:47:38 -0400

    [tcr]

--- spec/play_round_spec.rb
+++ spec/play_round_spec.rb
@@ -1,8 +1,8 @@
 class PlayRound
   
-  def call(get_input, claim)
+  def call(board, get_input, claim)
     space = get_input.()
-    claim.()
+    claim.(board)
   end
 
 end
@@ -12,14 +12,16 @@ describe PlayRound do
   let(:get_input) { double(:call => nil) }
   let(:claim)     { double(:call => nil) }
 
-  before { subject.(get_input, claim) }
+  let(:board) { double }
+
+  before { subject.(board, get_input, claim) }
 
   it "gets input" do
     expect(get_input).to have_received(:call)
   end
 
   it "claims space" do
-    expect(claim).to have_received(:call)
+    expect(claim).to have_received(:call).with(board)
   end
 
 end
