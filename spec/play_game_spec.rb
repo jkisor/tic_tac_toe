9fd16ff6b2df79c39dc0b821e8363b4ed210e43b
John Kisor <jkisor@gmail.com>
2019-05-12 15:49:16 -0400

    [tcr]

--- spec/play_game_spec.rb
+++ spec/play_game_spec.rb
@@ -1,22 +1,26 @@
 class PlayGame
   
-  def initialize(start_game)
+  def initialize(start_game, play_round)
     @start_game = start_game
+    @play_round = play_round
   end
   
   def call(board)
     @start_game.(board)
+
+    @play_round.(board)
   end
 
 end
 
 describe PlayGame do
 
-  subject { described_class.new(start_game) }
+  subject { described_class.new(start_game, play_round) }
 
   let(:start_game) { double(:call => nil) }
+  let(:play_round) { double(:call => nil) }
 
-  let(:board) { double }
+  let(:board) { double(:has_empty_spaces? => true) }
 
   before { subject.(board) }
   
@@ -24,6 +28,8 @@ describe PlayGame do
     expect(start_game).to have_received(:call).with(board)
   end
 
-  
+  it "plays round" do
+    expect(play_round).to have_received(:call).with(board)
+  end
 
 end
