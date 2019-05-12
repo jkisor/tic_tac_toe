3d187ce0a35b9e9052fd9e4a14dc1a9fd74dc29f
John Kisor <jkisor@gmail.com>
2019-05-12 15:45:59 -0400

    [tcr]

--- spec/play_game_spec.rb
+++ spec/play_game_spec.rb
@@ -1,17 +1,25 @@
 class PlayGame
   
+  def initialize(start_game)
+    @start_game = start_game
+  end
+  
   def call
-
+    @start_game.()
   end
 
 end
 
 describe PlayGame do
-  subject { described_class.new }
+
+  subject { described_class.new(start_game) }
+
+  let(:start_game) { double(:call => nil) }
 
   before { subject.() }
   
   it do
+    expect(start_game).to have_received(:call)
   end
 
 end
