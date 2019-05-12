a75ca55b120ffd9ef9c938116163c242079e572e
John Kisor <jkisor@gmail.com>
2019-05-12 08:00:57 -0400

    [tcr]

--- spec/show_board_spec.rb
+++ spec/show_board_spec.rb
@@ -1,7 +1,17 @@
 class ShowBoard
+  
+  def initialize(formatter)
+  end
+  
 end
 
 describe ShowBoard do
+
+  subject { described_class.new(formatter) }
+
+  let(:formatter) { double }
+  
   it do
   end
+
 end
