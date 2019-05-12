d27264adc057a409cb512f0df52af55cca2a1bce
John Kisor <jkisor@gmail.com>
2019-05-12 08:02:09 -0400

    [tcr]

--- spec/show_board_spec.rb
+++ spec/show_board_spec.rb
@@ -1,18 +1,24 @@
 class ShowBoard
   
-  def initialize(formatter)
+  def initialize(formatter, printer)
+    @formatter = formatter
+    @printer   = printer
   end
 
+  def call
+  end
+  
 end
 
 describe ShowBoard do
 
-  subject { described_class.new(formatter) }
+  subject { described_class.new(formatter, printer) }
 
   let(:formatter) { double }
   let(:printer)   { double }
   
   it do
+    subject.call
   end
 
 end
