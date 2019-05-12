078a74b5697d70c5e2ffc68f29b0f95537108090
John Kisor <jkisor@gmail.com>
2019-05-12 14:01:02 -0400

    [tcr]

--- spec/end_game_spec.rb
+++ spec/end_game_spec.rb
@@ -1,6 +1,21 @@
 class EndGame
+
+  def initialize(printer)
+  end
+
+  def call
+  end
+
 end
 
 describe EndGame do
+  
+  subject { described_class.new(printer) }
+
+  let(:printer) { double }
+
+  it do
+    subject.()
+  end
 
 end
