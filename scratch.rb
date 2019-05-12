fa54422a05505df648582300fa124587a63d6e8e
John Kisor <jkisor@gmail.com>
2019-05-12 13:19:46 -0400

    [tcr]

--- scratch.rb
+++ scratch.rb
@@ -10,24 +10,33 @@ require "./lib/board"
 require "./lib/show_board"
 require "./lib/play_round"
 
-rules      = Claim::Rules.new
-next_shape = Claim::NextShape.new
-commit     = Claim::Commit.new
+def claim
+  rules      = Claim::Rules.new
+  next_shape = Claim::NextShape.new
+  commit     = Claim::Commit.new
 
-claim = Claim.new(rules, next_shape, commit)
+  Claim.new(rules, next_shape, commit)
+end
+
+def show_board
+  formatter  = FormatBoard.new
+  printer    = Terminal::Print
+
+  ShowBoard.new(formatter, printer)
+end
+
+def play_round
+  PlayRound.new(
+    Terminal::GetInput, 
+    claim, 
+    show_board
+  )
+end
 
 NUM_SPACES = 9
 
 board = Board.new("-" * NUM_SPACES)
 space = 0
-
-formatter  = FormatBoard.new
-printer    = Terminal::Print
-show_board = ShowBoard.new(formatter, printer)
-
-get_input = Terminal::GetInput
-
-play_round = PlayRound.new(get_input, claim, show_board)
 
 ###
 
