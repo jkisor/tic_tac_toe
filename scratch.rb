701ff557e9bb487db00015387f6cda03392c44be
John Kisor <jkisor@gmail.com>
2019-05-13 06:43:09 -0400

    [tcr]

--- scratch.rb
+++ scratch.rb
@@ -15,44 +15,48 @@ require "./lib/end_game"
 
 require "./lib/play_game"
 
-def play_round
-  PlayRound.new(
-    Terminal::GetInput, 
-    claim, 
-    show_board
-  )
-end
+class Game
+  
+  def self.play(board)
+    PlayGame.new(start_game, play_round, end_game).(board)
+  end
 
-def claim
-  rules      = Claim::Rules.new
-  next_shape = Claim::NextShape.new
-  commit     = Claim::Commit.new
+  def self.play_round
+    PlayRound.new(
+      Terminal::GetInput, 
+      claim, 
+      show_board
+    )
+  end
+  
+  def self.claim
+    rules      = Claim::Rules.new
+    next_shape = Claim::NextShape.new
+    commit     = Claim::Commit.new
+  
+    Claim.new(rules, next_shape, commit)
+  end
+  
+  def self.show_board
+    formatter  = FormatBoard.new
+    printer    = Terminal::Print
+  
+    ShowBoard.new(formatter, printer)
+  end
+  
+  def self.start_game
+    StartGame.new(show_board)
+  end
+  
+  def self.end_game
+    printer = Terminal::Print
+  
+    EndGame.new(printer)
+  end
 
-  Claim.new(rules, next_shape, commit)
-end
-
-def show_board
-  formatter  = FormatBoard.new
-  printer    = Terminal::Print
-
-  ShowBoard.new(formatter, printer)
-end
-
-def start_game
-  StartGame.new(show_board)
-end
-
-def end_game
-  printer = Terminal::Print
-
-  EndGame.new(printer)
 end
 
 NUM_SPACES = 9
 board = Board.new("-" * NUM_SPACES)
 
-###
-play_game = PlayGame.new(start_game, play_round, end_game)
-play_game.(board)
-
-
+Game.play(board)
