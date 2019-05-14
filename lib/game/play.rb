587b647c974e0299669b04a82cf39f37b51521f6
John Kisor <jkisor@gmail.com>
2019-05-13 21:38:15 -0400

    [tcr]

--- lib/game/play.rb
+++ lib/game/play.rb
@@ -1,13 +1,13 @@
 module Game
 
   class Play
-    
+
     def initialize(start_game, play_round, end_game)
       @start_game = start_game
       @play_round = play_round
       @end_game   = end_game
     end
-    
+
     def call
 
       board = @start_game.()
