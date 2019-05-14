b3726eb28e156f3cb065446e41dc9192447a76e4
John Kisor <jkisor@gmail.com>
2019-05-14 06:56:37 -0400

    [tcr]

--- lib/game/play/loop.rb
+++ lib/game/play/loop.rb
@@ -3,20 +3,20 @@ module Game
   class Play
 
     class Loop
-      
+
       def initialize(get_input, claim, show_board)
         @get_input  = get_input
         @claim      = claim
         @show_board = show_board
       end
-    
+
       def call(board)
         space = @get_input.()
-    
+
         new_board = @claim.(board, space)
-        
+
         @show_board.(new_board)
-    
+
         new_board
       end
 
