5f5650f675be316e129ad33dca03ebe9c734c04e
John Kisor <jkisor@gmail.com>
2019-05-14 07:14:25 -0400

    [tcr]

---
+++ lib/game/play/loop/claim.rb
@@ -0,0 +1,23 @@
+module Game
+  class Play
+    class Loop
+      class Claim
+
+        def initialize(rules, next_shape, commit)
+          @rules      = rules
+          @next_shape = next_shape
+          @commit     = commit
+        end
+
+        def call(board, space)
+          return board unless @rules.allow?(board, space)
+
+          shape = @next_shape.(board)
+          @commit.(board, space, shape)
+        end
+
+      end
+
+    end
+  end
+end
