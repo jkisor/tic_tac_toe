6129e8d20368bd6c71b6ac4a8f4a136a3231a3fc
John Kisor <jkisor@gmail.com>
2019-05-16 07:24:45 -0400

    [tcr]

--- lib/game.rb
+++ lib/game.rb
@@ -4,13 +4,9 @@ module Game
   def self.play
     Play.new(
       Game::Play::Start.build, 
-      loop_game, 
+      Play::Loop.build, 
       Game::Play::End.build
     ).()
-  end
-
-  def self.loop_game
-    Play::Loop.build
   end
 
 end
